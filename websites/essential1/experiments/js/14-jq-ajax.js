f4d3ef948283f2a091766570c10b92b1ffedbf9d
Asad Shahabuddin <asad.shahabuddin@gmail.com>
2015-02-07 22:06:11 -0500

    Week 3 Experiments - Descriptions Added
    [2015-02-07] Week 3 Experiments - Descriptions Added

--- websites/essential1/experiments/js/14-jq-ajax.js
+++ websites/essential1/experiments/js/14-jq-ajax.js
@@ -7,7 +7,7 @@ $(".imdbBtn").click(function()
 {
     $.ajax(
     {
-        url: "http://www.myapifilms.com/imdb?title=swimming%20with%20sharks&format=JSONP&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=N&exactFilter=0&limit=1&lang=en-us&actors=N&biography=0&trailer=0&uniqueName=0&filmography=0&bornDied=0&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N",
+        url: "http://www.myapifilms.com/imdb?title=Lilya%204-Ever&format=JSONP&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=N&exactFilter=0&limit=1&lang=en-us&actors=N&biography=0&trailer=0&uniqueName=0&filmography=0&bornDied=0&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N",
         dataType: "jsonp",
         success: function(response)
         {
